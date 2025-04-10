e0db25b3a7dce22105a32f27270c2f412573cbc0
monitarvind <monit.kk@gmail.com>
2025-04-11 01:02:01 +0530

    added aws provider to main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+# AWS Provider
+
+provider "aws" {
+  region = "ap-south-1"
+access_key = "AKIAZ5CPGAHUTSAXOKUI"
+secret_key = "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"
+
+}
+
+
+#S3 bucket create AWS
+
+
